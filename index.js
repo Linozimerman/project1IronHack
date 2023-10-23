8eb38a1b5d41d6f74d34d679e869fca83d0edbd2
lino zimerman <linozimerman@gmail.com>
2023-10-23 15:56:05 +0200

    Got the nested forEach working

--- index.js
+++ index.js
@@ -1,39 +1,36 @@
-const divClassWindows = document.querySelector("#spacesWindows")
+/* const divClassWindows = document.querySelector("#spacesWindows")
 const div00 = document.querySelector(".item0-0")
 const div01 = document.querySelector(".item0-1")
 const div10 = document.querySelector(".item1-0")
-const div11 = document.querySelector(".item1-1")
+const div11 = document.querySelector(".item1-1") */
+const boardGameElement = document.querySelector("#div-boardGame")
+const items = document.querySelector(".item")
 
 //const dinamicSelector = document.querySelector(`.item-${element}-${innerelement}`)
 
 gameBoardArray = [
     [0,2],
-    [0,1]
+    [2,1]
 ]
 
-//console.log (gameBoardArray)
 
-/*
-let empty = "";
-let playerHTML = "";
-let enemy = "";
-let friend = "";
-*/
 
-gameBoardArray.forEach(element => {
-    console.log("First console>",element)
-    element.forEach(innerElement =>{
-        console.log("Second console",innerElement)
-        const dinamicSelector = document.querySelector(`.item${element}-${innerElement}`)
 
-        if(gameBoardArray[element][innerElement] === 0){
-            dinamicSelector.className = "nothingClass"
-        } else if(gameBoardArray[element][innerElement] === 1){
-            dinamicSelector.className = "playerClass"
-        } else if (gameBoardArray[element][innerElement] === 2){
-            dinamicSelector.className = "enemyClass"
-        } else if (gameBoardArray[element][innerElement] === 3){
-            dinamicSelector.className = "peopleClass"
+
+gameBoardArray.forEach((element,rowIndex) => {
+    console.log("First console","element:",element, "rowIndex:",rowIndex)
+    element.forEach((innerElement, columnIndex) =>{
+        console.log("Second console innerElement:",innerElement, "columnIndex:", columnIndex)
+        const dinamicSelector = document.querySelector(`.item${rowIndex}\\-${columnIndex}`)
+        console.log("Este es el dinamicSelector:", dinamicSelector);
+        if(innerElement === 0){
+            dinamicSelector.classList.add("nothingClass")
+        } else if(innerElement === 1){
+            dinamicSelector.classList.add("playerClass")
+        } else if (innerElement === 2){
+            dinamicSelector.classList.add("enemyClass")
+        } else if (innerElement === 3){
+            dinamicSelector.classList.add("peopleClass")
         }
         
     });
